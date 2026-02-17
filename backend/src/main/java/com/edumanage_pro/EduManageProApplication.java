6631d543b510728701167487da80dc12749826be
Akash Kulkarni <akashkulkarni@outlook.in>
2026-02-17 16:44:28 +0530

    SpringBoot and React intialized

---
+++ backend/src/main/java/com/edumanage_pro/EduManageProApplication.java
@@ -0,0 +1,13 @@
+package com.edumanage_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManageProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManageProApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edumanage_pro/EduManageProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumanage_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManageProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
